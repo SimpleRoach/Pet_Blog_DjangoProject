4755f027248bc08bcc292a710ca1f80c050020e3
text <tlsmscience@gmail.com>
2024-04-17 14:16:27 +0500

    2

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,11 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(verbose_name='Название статьи', max_length=100)
     text = models.TextField(verbose_name='Основной текст статьи')
-    date = models.DateField(verbose_name='Дата', default=timezone.now)
+    date = models.DateTimeField(verbose_name='Дата', default=timezone.now)
     avtor = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title}'
 
     class Meta:
         verbose_name = 'Новость'
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
+from .models import News
 
 
 def home(request):
-    number = 1
-
-    news = [
-        {
-            'title': 'Главная новость',
-            'text': 'Самая первая версия сайта',
-            'date': '16.04.24',
-            'avtor': 'admin'
-        },
-        {
-            'title': 'Дополнение',
-            'text': 'Сейчас просто знакомлюсь с функционалом фреймворка Django',
-            'date': '16.04.24',
-            'avtor': 'admin'
-        }
-    ]
     data = {
-        'news': news,
+        'news': News.objects.all(),
         'title': 'Global page',
     }
     return render(request, 'blog/home.html', data)
