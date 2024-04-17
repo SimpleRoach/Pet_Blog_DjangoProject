8b31ede684cc0c923f80464c9c20c040f66c5705
text <tlsmscience@gmail.com>
2024-04-17 17:14:14 +0500

    add_registration

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
+    # 'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
---
+++ users/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+# from django.contrib.auth.forms import UserCreationForm
+
+
+# MyRegUsersForm
+class MyRegUsersForm(forms.Form):
+    email = forms.EmailField(required=True,
+                             max_length=254,
+                             widget=forms.TextInput(attrs={
+                                 'placeholder': 'Email',
+                                 'class': 'form-control'}))
+    username = forms.CharField(required=True,
+                               widget=forms.TextInput(attrs={
+                                   'placeholder': 'Username',
+                                   'class': 'form-control'}))
+    first_name = forms.CharField(required=True,
+                                 widget=forms.TextInput(attrs={
+                                     'placeholder': 'Имя',
+                                     'class': 'form-control'}))
+    last_name = forms.CharField(required=True,
+                                widget=forms.TextInput(attrs={
+                                    'placeholder': 'Фамилия',
+                                    'class': 'form-control'}))
+    password1 = forms.CharField(required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'placeholder': 'Пароль',
+                                    'class': 'form-control'}))
+    password2 = forms.CharField(required=True,
+                                widget=forms.PasswordInput(attrs={
+                                    'placeholder': 'Подтвердите пароль',
+                                    'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['email',
+                  'username',
+                  'first_name',
+                  'last_name',
+                  'password1',
+                  'password2'
+                  ]
