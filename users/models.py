68cd7d7fd2bd1e777183ababf1cb7be126e031c4
Ramil <tlsmscience@gmail.com>
2024-06-11 09:06:19 +0500

    Добавил в настройках (в settings.py)  отдельную папку для медиа

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField('Фото', default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField('Фото', default='default.png', upload_to='profile_pics')
 
     def __str__(self):
         return f'Личный кабинет пользователя {self.user.username}'
